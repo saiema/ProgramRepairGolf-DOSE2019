873d07480df0342908985fa1f787e1db05645482
cadafe <cadafe12@gmail.com>
2019-11-14 11:46:05 -0300

    working on scenarios

--- src/test/java/unrc/dose/ChallengeStatSteps.java
+++ src/test/java/unrc/dose/ChallengeStatSteps.java
@@ -9,28 +9,33 @@ import unrc.dose.User;
 import unrc.dose.ChallengeStat;
 
 public class ChallengeStatSteps extends StepUtils{
+    private static User u = new User();
+    private static Challenge ch = new Challenge();
+    private static ChallengeStat cSt = new ChallengeStat();
+    private static int challengeId;
 
     @Given("^that a user with id (\\d+) is already logged on$")
-    public void that_a_user_with_id_is_already_logged_on(int arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public boolean that_a_user_with_id_is_already_logged_on(int arg1) {
+        u = User.findFirst("id = ?", arg1);
+        return u != null;
     }
 
     @Given("^exists a challenge with id (\\d+)$")
-    public void exists_a_challenge_with_id(int arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public boolean exists_a_challenge_with_id(int arg1) {
+        ch = Challenge.findFirst("id = ?", arg1);
+        return ch != null;
     }
 
     @Given("^it was solved by a least one user$")
-    public void it_was_solved_by_a_least_one_user() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public boolean it_was_solved_by_a_least_one_user() {
+        challengeId = ch.getInteger("id");
+        cSt = ChallengeStat.findFirst("id = ?", challengeId);
+        return (cSt.getInteger("solved_count") > 0);
     }
 
-    @When("^(.*) sends the challenge's id (\\d+) to the system$")
-    public void sends_the_challenge_s_id_to_the_system(String arg1, int arg2) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
+    @When("^the user inputs the challenge's id (\\d+) to the system$")
+    public void the_user_inputs_the_challenge_s_id_to_the_system(int arg1) {
+        
         throw new PendingException();
     }
 
--- src/test/resources/unrc/dose/ChallengeStat.feature
+++ src/test/resources/unrc/dose/ChallengeStat.feature
@@ -4,5 +4,5 @@ Feature: challenge stats
     Given that a user with id 10 is already logged on
     And exists a challenge with id 15
     And it was solved by a least one user
-    When <User> sends the challenge's id 15 to the system
+    When the user inputs the challenge's id 15 to the system
     Then the system will show the corresponding stats
