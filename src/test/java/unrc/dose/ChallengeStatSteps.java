2bf8bb11cfaa2e39705109d0279f43d32fbf0f33
MarianoManzetti <mariano.manzetti@gmail.com>
2019-11-14 20:33:50 -0300

    working on scenarios

--- src/test/java/unrc/dose/ChallengeStatSteps.java
+++ src/test/java/unrc/dose/ChallengeStatSteps.java
@@ -7,16 +7,18 @@ import cucumber.api.java.en.When;
 import unrc.dose.StepUtils;
 import unrc.dose.User;
 import unrc.dose.ChallengeStat;
+import unrc.dose.Challenge;
 
 public class ChallengeStatSteps extends StepUtils{
     private static User u = new User();
     private static Challenge ch = new Challenge();
     private static ChallengeStat cSt = new ChallengeStat();
-    private static int challengeId;
+    private static double statistics;
+    private static int resolved;
 
-    @Given("^that a user with id (\\d+) is already logged on$")
-    public boolean that_a_user_with_id_is_already_logged_on(int arg1) {
-        u = User.findFirst("id = ?", arg1);
+    @Given("^the user \"([^\"]*)\" with id (\\d+) has already logged in$")
+    public boolean the_user_with_id_has_already_logged_in(String arg1, int arg2) {
+        u = User.findFirst("id = ?", arg2);
         return u != null;
     }
 
@@ -28,21 +30,21 @@ public class ChallengeStatSteps extends StepUtils{
 
     @Given("^it was solved by a least one user$")
     public boolean it_was_solved_by_a_least_one_user() {
-        challengeId = ch.getInteger("id");
-        cSt = ChallengeStat.findFirst("id = ?", challengeId);
+        cSt = ChallengeStat.findFirst("id = ?", ch.getId());
         return (cSt.getInteger("solved_count") > 0);
     }
 
     @When("^the user inputs the challenge's id (\\d+) to the system$")
     public void the_user_inputs_the_challenge_s_id_to_the_system(int arg1) {
-        
-        throw new PendingException();
+        cSt = ChallengeStat.getChallengeStat(arg1);
+        statistics = cSt.getDouble("average_score");
+        resolved = cSt.getInteger("solved_count");
     }
 
     @Then("^the system will show the corresponding stats$")
     public void the_system_will_show_the_corresponding_stats() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        System.out.println("%" + statistics);
+        System.out.println(resolved);
     }
 
 }
--- src/test/resources/unrc/dose/ChallengeStat.feature
+++ src/test/resources/unrc/dose/ChallengeStat.feature
@@ -1,8 +1,8 @@
-Feature: challenge stats
+Feature: Challenge Stats
 
     Scenario: A user wants to see a challenge's stat
-    Given that a user with id 10 is already logged on
-    And exists a challenge with id 15
-    And it was solved by a least one user
-    When the user inputs the challenge's id 15 to the system
-    Then the system will show the corresponding stats
+        Given the user "Sabalero" with id 15 has already logged in
+        And exists a challenge with id 10
+        And it was solved by a least one user
+        When the user inputs the challenge's id 10 to the system
+        Then the system will show the corresponding stats
