276ccbd7ce8885c2eeb7980f3d8f05e12f85bb59
cadafe <cadafe12@gmail.com>
2019-11-23 13:47:08 -0300

    new CS method and it respective endpoint/action

--- client/src/actions/challengeStatActions/challengeStatActions.js
+++ client/src/actions/challengeStatActions/challengeStatActions.js
@@ -2,8 +2,48 @@ import axios from 'axios';
 import {
   FETCH_CHALLENGESTAT_REQUEST,
   FETCH_CHALLENGESTAT_SUCCESS,
-  FETCH_CHALLENGESTAT_FAILURE
-} from '../constants/CSActionTypes'
+  FETCH_CHALLENGESTAT_FAILURE,
+  FETCH_CHALLENGESTATS_REQUEST,
+  FETCH_CHALLENGESTATS_SUCCESS,
+  FETCH_CHALLENGESTATS_FAILURE
+} from '../../constants/challengeStatConstants/ActionTypes'
+
+const fetchChallengeStatsRequest = () => {
+  return {
+    type: FETCH_CHALLENGESTATS_REQUEST
+  }
+}
+
+const fetchChallengeStatsSucess = users => {
+    return {
+        type: FETCH_CHALLENGESTATS_SUCCESS,
+        payload: challengeStats
+    }
+}
+
+const fetchChallengeStatsFailure = error => {
+    return {
+        type: FETCH_CHALLENGESTATS_FAILURE,
+        payload: error
+    }
+}
+
+export const fetchChallengeStats = () => {
+  return function(dispatch, getState) {
+    if (getState().challengeStats.data.length === 0) {
+      dispatch(fetchChallengeStatsRequest())
+
+      axios.get('http://localhost:55555/challengestat/all')
+        .then( res =>{
+          console.log(res.data);
+          dispatch(fetchChallengeStatsSucess(res.data))
+        })
+        .catch(error => {
+          dispatch(fetchChallengeStatsFailure(error.message))
+        })
+    }
+  }
+}
 
 
 const fetchChallengeStatRequest = () => {
