d977d97b21aa3845d85d11a37027ee7262491482
Agustin <agustinborda1998@gmail.com>
2019-11-24 21:04:51 -0300

    #181 added components for IndividualStats

---
+++ client/src/components/userstat/IndividualStats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const IndividualStats = ({ stats }) => {
+    console.log("IndividualStats");
+    console.log(stats);
+    console.log("**************");
+
+    return (
+      <div className="stat-list">
+      </div>
+    );
+  }
+
+  export default IndividualStats
