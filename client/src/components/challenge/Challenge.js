c3f5d3a9f5f9f515e707e20465ff4699dbfacf79
cuestalvaro <alvarosergiocuesta@gmail.com>
2019-11-25 22:59:32 -0300

    change Challenge.js

--- client/src/components/challenge/Challenge.js
+++ client/src/components/challenge/Challenge.js
@@ -1,105 +1,52 @@
 import React , {Component} from 'react'
-import { connect } from 'react-redux'
-//import AddChallenge from './AddChallenge'
-import TableCompilationChallenge from './TableCompilationChallenge'
-import TableTestChallenge from './TableTestChallenge'
-import { 
-  fetchAllCompilationChallenge,
-  fetchResolvedCompilationChallenge,
-  fetchUnsolvedCompilationChallenge
-} from '../../actions/compilationChallengeActions'
-import { 
-  fetchAllTestChallenge,
-  fetchResolvedTestChallenge,
-  fetchUnsolvedTestChallenge
-} from '../../actions/testChallengeActions'
+import AddChallenge from './AddChallenge'
+import ViewChallenge from './ViewChallenge'
 
 class Challenge extends Component {
 
-  componentDidMount() {
-    this.props.fetchAllCompilationChallenge();
-    this.props.fetchResolvedCompilationChallenge();
-    this.props.fetchUnsolvedCompilationChallenge();
-    this.props.fetchAllTestChallenge();
-    this.props.fetchResolvedTestChallenge();
-    this.props.fetchUnsolvedTestChallenge();
+  state = {
+    opc: ""
   }
 
-
-  render(){
-    return (
-        <div className="container">
-            
-          <p>all compilation challenge</p>
-          <TableCompilationChallenge
-            listCompilationChallenge={this.props.allCompilationChallenge}
-          />
-
-          {/* <p>unsolved compilation challenge</p>
-          <TableCompilationChallenge
-            listCompilationChallenge={this.props.unsolvedCompilationChallenge}
-          />
-
-          <p>resolved compilation challenge</p>
-          <TableCompilationChallenge
-            listCompilationChallenge={this.props.resolvedCompilationChallenge}
-          />  */}
-
-          <p>all test challenge</p>
-          <TableTestChallenge
-            listTestChallenge={this.props.allTestChallenge}
-          />
-
-          {/* <p>unsolved test challenge</p>
-          <TableTestChallenge
-            listTestChallenge={this.props.unsolvedTestChallenge}
-          />
-
-          <p>resolved test challenge</p>
-          <TableTestChallenge
-            listTestChallenge={this.props.resolvedTestChallenge}
-          /> */}
-
-          {/* <AddChallenge/> */}
-
+  show(){
+    if (this.state.opc === "add") {
+      return (
+        <div>
+          { <AddChallenge/> }
+        </div>
+      );
+    }
+    else if (this.state.opc==="view"){
+      return (
+        <div>
+          { <ViewChallenge/> }
+        </div>
+      );
+    }
+    else {
+      return (
+        <div >
+           <div className="block-button"> 
+            <h1> Challenge </h1> 
+          </div>
+           <div className="block-button"> 
+            <button className="button-submit" onClick={() => this.setState({opc:"add"})}>add challenge</button>
+          </div>
+          <div className="block-button"> 
+            <button className="button-submit" onClick={() => this.setState({opc:"view"})}>view challenge</button>
+          </div>
         </div>
       )
-  }
-
-}
-
-const mapStateToProps = (state) => {
-  return {
-    allCompilationChallenge: state.allCompilationChallenge.data,
-    resolvedCompilationChallenge: state.resolvedCompilationChallenge.data,
-    unsolvedCompilationChallenge: state.unsolvedCompilationChallenge.data,
-    allTestChallenge: state.allTestChallenge.data,
-    resolvedTestChallenge: state.resolvedTestChallenge.data,
-    unsolvedTestChallenge: state.unsolvedTestChallenge.data,
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchAllCompilationChallenge: () => {
-      dispatch(fetchAllCompilationChallenge())
-    },
-    fetchResolvedCompilationChallenge: () => {
-      dispatch(fetchResolvedCompilationChallenge())
-    },
-    fetchUnsolvedCompilationChallenge: () => {
-      dispatch(fetchUnsolvedCompilationChallenge())
-    },
-    fetchAllTestChallenge: () => {
-      dispatch(fetchAllTestChallenge())
-    },
-    fetchResolvedTestChallenge: () => {
-      dispatch(fetchResolvedTestChallenge())
-    },
-    fetchUnsolvedTestChallenge: () => {
-      dispatch(fetchUnsolvedTestChallenge())
     }
   }
+  
+  render () {
+    return (
+      <div>
+        {this.show()}
+      </div>
+    );
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Challenge)
+export default Challenge
