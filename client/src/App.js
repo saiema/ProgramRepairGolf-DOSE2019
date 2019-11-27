dfd4adc57e52c0311e79963607d202f29b38c17f
MarianoManzetti <mariano.manzetti@gmail.com>
2019-11-27 18:16:53 -0300

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import HackersContainer from './components/HackersContainer';
 import Hacker from './components/Hacker';
-import ChallengeStat from './components/challengeStatComponents/ChallengeStat'
+import UserStats from './components/userstat/UserStats'
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar'
 import Challenge from './components/challenge/Challenge'
 import PrivateRoute from './components/PrivateRoute';
 import ResetPass from './components/componentsUser/EmailContainer';
+import ChallengeStat from './components/challengeStatComponents/ChallengeStat'
 
 
 class App extends Component {
@@ -17,17 +18,16 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-
           <Switch>
             <Route exact path='/' component={Home}/>
             <Route path='/about' component={About} />
+            <Route path='/userstats' component={UserStats} />
             <Route path='/hackers' component={HackersContainer} />
             <Route path="/challenge" component={Challenge} />
             <Route exact path='/resetPassword' component={ResetPass}/>
-            <Route path="/stats" component={ChallengeStat} ></Route>
+            <Route path="/challengestat" component={ChallengeStat} />
             <PrivateRoute path='/hackers' component={HackersContainer} />
             <Route path="/:hacker_id" component={Hacker} ></Route>
-            
           </Switch>
         </div>
       </BrowserRouter>
