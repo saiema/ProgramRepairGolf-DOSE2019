32f4e4a949b896d01fa667d72ed3259d618c7058
matias <mformento8@gmail.com>
2019-12-02 19:01:40 -0300

    change in table challenges

--- src/migrations/20191130185257_alter_table_challenges_addUnique.sql
+++ src/migrations/20191130185257_alter_table_challenges_addUnique.sql
@@ -2,7 +2,7 @@ alter table challenges
     modify user_id integer not null,
     modify class_name varchar(30) not null unique,
     modify title varchar (50) not null unique,
-    modify description varchar(50) unique,
+    modify description varchar(50),
     modify source varchar(10000) not null,
     modify point integer not null,
     modify owner_solution_id integer not null;
