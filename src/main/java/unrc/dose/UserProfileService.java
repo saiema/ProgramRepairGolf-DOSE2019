93742952f75087a928b639972537001a81fc8eb5
Mariana-Ayup <mariayup@gmail.com>
2020-02-26 15:48:06 -0300

    User existence and user profile check

--- src/main/java/unrc/dose/UserProfileService.java
+++ src/main/java/unrc/dose/UserProfileService.java
@@ -10,7 +10,14 @@ public final class UserProfileService {
   *Invokes the method which creates a user profile and transforms to json.
   **/
   public static String createProfile(Integer userId, String displayName, String twitterId) {
-	  
+	  if (!User.exists(userId)) {
+				throw new UserNotFoundException(String.valueOf(userId));
+			}
+  	  UserProfile up = UserProfile.findFirst("user_id = ?", userId);
+  	  if(up != null){
+  		  throw new IllegalArgumentException ("The user profile already exists");
+  	  }
+  	
       return UserProfile.createUserProfile(userId, displayName, twitterId).toJson(true);
 
   }
@@ -21,9 +28,11 @@ public final class UserProfileService {
    */
   
   public static String getProfile(int userId) {
-	  
-		  UserProfile up = UserProfile.findProfile(userId);
-		  return up.toJson(true, "user_id", "displayName","twitter_id");
+	  if (!User.exists(userId)) {
+			throw new UserNotFoundException(String.valueOf(userId));
+			}
+	  UserProfile up = UserProfile.findProfile(userId);
+	  return up.toJson(true, "user_id", "displayName","twitter_id");
  }
   /**
    * Invokes a method that finds a user profile given an  user id and modifies it and transforms to json.
@@ -34,7 +43,9 @@ public final class UserProfileService {
    */
   
  public static String updateProfile (int userId, String displayName,String twitterId) {
-    
+	if (!User.exists(userId)) {
+			throw new UserNotFoundException(String.valueOf(userId));
+			}
 	UserProfile up = UserProfile.findProfile(userId); 
 	up.setDisplayName("displayName");
 	up.setTwitterId("twitterId");
